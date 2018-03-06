3603d8a7321b13a3179badd983908c0dacc167a2
guillermodejuan <dejuanguillermo@gmail.com>
2018-03-06 12:42:19 +0100

    correcion build

--- app/src/main/java/com/cta/android/hola_mundo_git/MainActivity.java
+++ app/src/main/java/com/cta/android/hola_mundo_git/MainActivity.java
@@ -8,20 +8,14 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Hola Mundo Mariano", Toast.LENGTH_LONG).show();
-            }
-        });
+
 
     }
 }
